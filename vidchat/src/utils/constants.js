b8503e5c4d7b470de93de5ec01a4bd516ea830a0
shubham <shubhamkumarjha564@gmail.com>
2025-07-25 11:25:08 +0530

    live link update

--- vidchat/src/utils/constants.js
+++ vidchat/src/utils/constants.js
@@ -2,7 +2,7 @@ export const GOOGLE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
  export const YOUTUBE_API_KEY = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API = "https://vid-chat-d2os.onrender.com/api/suggestions?q=";
+export const YOUTUBE_SEARCH_API = "https://vid-chat-v69j.onrender.com/api/suggestions?q=";
 
 
 
